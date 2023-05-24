40852719ce7f4146156bd2d176571a0dd3568f2a
tanmoyxxiv <dastanmoy24012000@gmail.com>
2023-05-24 11:56:06 +0530

    Added BaseController and ConnectionString

---
+++ LoggingDemo/Controllers/BaseController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace LoggingDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected readonly ILogger<BaseController> _logger;
+        protected readonly IConfiguration _config;
+
+
+        public BaseController(ILogger<BaseController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        protected void LogInformation(string message)
+        {
+            _logger.LogInformation(message);
+        }
+        protected void LogError(string message)
+        {
+            _logger.LogError(message);
+        }
+        protected void LogWarning(string message)
+        {
+            _logger.LogWarning(message);
+        }
+
+        protected SqlConnection GetSqlConnection()
+        {
+            try
+            {
+                string connectionString = _config.GetConnectionString("MyDbConnection");
+                if (string.IsNullOrEmpty(connectionString))
+                {
+                    throw new Exception("Connection string not found in appsettings.json.");
+                }
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error connecting to database.", ex);
+            }
+        }
+    }
+}
--- LoggingDemo/Controllers/CalculateController.cs
+++ LoggingDemo/Controllers/CalculateController.cs
@@ -1,28 +1,26 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace LoggingDemo.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CalculateController : ControllerBase
+    public class CalculateController : BaseController
     {
-        private readonly ILogger<CalculateController> _logger;
-
-        public CalculateController(ILogger<CalculateController> logger)
+        public CalculateController(ILogger<BaseController> logger, IConfiguration config) : base(logger,config)
         {
-            _logger = logger;
         }
 
 
         [HttpGet("/divide")]
         public IActionResult GetDivisionResult(int? a, int? b)
         {
-            _logger.LogInformation("Requested GetDivisionResult()");
+            LogInformation("Requested GetDivisionResult()");
 
             if (!a.HasValue || !b.HasValue)
             {
-                _logger.LogWarning("Incomplete Input for Division Operation");
+                LogWarning("Incomplete Input for Division Operation");
                 return BadRequest("Please enter value for both 'a' and 'b' : ");
             }
             try
@@ -30,14 +28,33 @@ namespace LoggingDemo.Controllers
                 int c = (int)(a / b);
                 var result = a + "/" + b + " = " + c;
 
-                _logger.LogInformation("GetDivisionResult() Request Successful with value {@Result}", result);
+                LogInformation("GetDivisionResult() Request Successful with value {@Result} " + result);
 
                 return Ok(result);
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex.Message);
+                LogError(ex.Message);
                 return StatusCode(500);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            using (SqlConnection connection = GetSqlConnection())
+            {
+                SqlCommand command = new SqlCommand($"SELECT * FROM MyTable WHERE Id = {id}", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    string result = reader.GetString(1) + " " + reader.GetString(2); 
+                    return Ok(result);
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
         }
     }
